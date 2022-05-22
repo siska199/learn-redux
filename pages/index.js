15d4c78b30a0bce97a72ce629bda13ed91728825
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-05-22 17:30:50 +0700

    setup initial app for learn redux

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
 import Head from "next/head";
-import { Card } from "../components/Card";
-import { Form } from "../components/Form";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
-
-  
   return (
     <div className={styles.container}>
       <Head>
         <title>Lesson1 Redux</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Form />
-        <Card />
-      </main>
+      <main></main>
     </div>
   );
 }
