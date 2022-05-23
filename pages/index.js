22489c01c05d638ca5e95554e9555727a4bbf58a
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-05-23 10:06:50 +0700

    clean setup (initialize project)

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Lesson1 Redux</title>
+        <title>Redux</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main></main>
